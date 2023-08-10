8a47c172731837070aa5879e1290858eb80902d0
mukeshx12 <mukeshyadv85288@gmail.com>
2023-08-10 16:51:43 +0530

    first

---
+++ app.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('button');
+const screenDisplay = document.querySelector('.screen');
+console.log(screenDisplay.textContent)
+
+let calculation = [];
+let accumulativeCalculation
+
+function calculate (button) {
+    const value = button.textContent
+    if(value==="C" || value==="AC") {
+        calculation =[]
+        screenDisplay.textContent = '.'
+    } else if (value=== "=") {
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+    accumulativeCalculation = calculation.join('');
+    screenDisplay.textContent = accumulativeCalculation
+    } 
+    
+    
+}
+
+buttons.forEach(button => button.addEventListener('click', ()=> calculate(button)))
+
